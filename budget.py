d13bab436e44633be6d3e0b5862511fdb1228c37
Gillian Law <gillianlaw@techliterate.co.uk>
2021-04-06 09:42:55 +0200

    Added balance check and funds check

--- budget.py
+++ budget.py
@@ -5,43 +5,38 @@ class Category():
         self.ledger = []
         self.amount = 0
 
-        
+
     def deposit(self, amount_added, decription = ''):
-        if not description:
-            description = ''
+        """adds money to that category"""
         self.ledger.append({'amount' : amount_added, 'description' : description})
         self.amount += amount_added
-        return True
+        return self
 
-      # note "amount": amount, "description": description od what has been spent, add to ledger list
+      # note "amount": amount, "description": description od what has been spent, add to ledger lis
+    def check_funds(self, amount_compare):
+        if self.amount < amount_compare:
+            return False
+        else:
+            return True
 
     def withdraw(self, amount_spent, description = ''):
+        """subtracts money, after checking there is enough"""
         if not self.check_funds(amount_spent):
             return False
-        if not description:
-            descrition = ''
         self.ledge.append({'amount' : (amount_spent * -1), 'description' : description})
         self.amount -= amount_spent
         return True
 
 
     def get_balance(self):
-      # this needs to go through and identify categories and add/ subtract amounts in each
-        pass
-
-    def transfer(self):
-      # move from one category to another, but only if there's enough
-        pass
-
-    def check_funds(self):
-      # this is used by both withdraw and transfer - checking there's enough money
-        pass
+        return self.amount
 
 
+    def transfer(self, amount_sent, transfer_to):
+        if not self.check_funds(amount_sent):
+            return False
+        self.withdraw(amount_sent, ("Transfer to " + self.category))
+        transfer_to.deposit(amount_sent, ("Transfer from " + self.category)
+        return True
 
-  # et cetera  #
-
-
-
-def create_spend_chart(categories):
-  # this needs a list of all catoegories, and ... a string creating a bar chart. No idea at the moment how to do that!
+# now need to add string commands before it'll work! 
