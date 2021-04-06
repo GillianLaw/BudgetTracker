124203443012a6873f945b66d20ce78f22153915
Gillian Law <gillianlaw@techliterate.co.uk>
2021-04-06 14:42:34 +0200

    Getting there. Graphs don't quite work

--- budget.py
+++ budget.py
@@ -1,9 +1,12 @@
+import math
+
 class Category():
     """defines the type of expense being tracked"""
     def __init__(self, category):
         self.category = category
         self.ledger = []
         self.amount = 0
+        self.spent = 0
 
 
     def deposit(self, amount_added, description=False):
@@ -59,9 +62,50 @@ class Category():
 
 def split_word(str):
   return [char for char in str]
-        #     body += items['description'] + (' ' * spaces_between) + f_amount + '\n'
-        # message = "Total: {}"
-        # total = message.format(self.get_balance())
-        # return header + body + total
+
+
+def create_spend_chart(arg):
+    print("Percentage spent by category")
+    num = len(arg)
+    totalSpent = 0
+    wordList = []
+    for item in range(num):
+        wordList.append(list(arg[item].category))
+        totalSpent += arg[item].spent
+
+    pctList = []
+    for item in range(num):
+        if totalSpent >= 1:
+            pctList.append(math.floor(arg[item].spent/totalSpent * 10))
+            ht = 10
+            while ht >= 0:
+                print(f"{(ht*10):3d}| ", end = "")
+                for p in range(num):
+                    if pctList[p-1] >= ht:
+                        print("o  ",end ="")
+                    else:
+                        print("",end="   ")
+                print()
+                ht -= 1
+            print(" "*4+"-"*(num*3+1))#bottom axis
+
+        l= len(max(wordList, key=len))
+        x=0
+        while x < l:
+            print("     ",end="")
+            i=0
+            while i < num:
+                try:
+                    print(wordList[i][x],end = "  ")
+                except IndexError:
+                    print("   ", end="")
+                i+=1
+            print("")
+            x+=1
+        return
+
+
+
+
 
 # now need to add string commands before it'll work!
